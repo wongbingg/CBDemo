209cdd474827da1eb01b0f5f5b86aad10dbfe819
WonbeenLee <wblee@autocrypt.io>
2025-05-08 15:38:07 +0900

    Initial Commit

---
+++ CBDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CBDemo
+//
+//  Created by 이원빈 on 5/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
